0ae3fbe1100faf6632e522a0ca130d8a562c29d8
Nathan Roy <nathanmroy@yahoo.com>
2020-01-30 19:10:27 -0800

    added user call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const API = 'https://acme-users-api-rev.herokuapp.com/api';
+
+const fetchUser = async () => {
+	const storage = window.localStorage;
+	const userId = storage.getItem('userId');
+	if (userId) {
+		try {
+			return (await axios.get(`${API}/users/detail/${userId}`)).data;
+		} catch (ex) {
+			storage.removeItem('userId');
+			return fetchUser();
+		}
+	}
+	const user = (await axios.get(`${API}/users/random`)).data;
+	storage.setItem('userId', user.id);
+	return user;
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ user, setUser ] = useState({});
+
+	useEffect(() => {
+		fetchUser().then((user) => setUser(user));
+	}, []);
+
+	console.log(user);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div>{user.avatar}</div>
+				<div>{`Welcome ${user.fullName}`}</div>
+				<button>Change User</button>
+			</header>
+		</div>
+	);
 }
 
 export default App;
